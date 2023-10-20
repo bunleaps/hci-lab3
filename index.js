deb139f0275233da6cd919eee5851ff8d2f4bc3f
bunleaps <bunleap.sorn@gmail.com>
2023-10-21 01:00:43 +0700

    idk

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const __dirname = path.dirname(__filename);
 //   }, 100);
 // });
 
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
